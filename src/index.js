83c2d4e4ee6a8e5d334245e4c55f6f850bda707c
Valov Ivan <pechenkooo3@gmail.com>
2023-02-28 17:32:05 +0200

    11

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-// import './sass/index.scss';
 import './js/header.js';
 import './js/modalCard';
 import './js/localStorage';
 import './js/input.js';
 
 import './js/change-theme.js';
-// import './js/library.js';
+
 import './js/popular-films.js';
 import './js/modal_film.js';
 import './js/popular-films.js';
